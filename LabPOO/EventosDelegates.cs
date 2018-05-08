fcb7cbc7d331253ac005de9e3c4c8880ee9800f3
ING <jcfiguero@miuandes.cl>
2018-05-08 16:34:18 -0300

    lab 7 johnny donoso

---
+++ LabPOO/EventosDelegates.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabPOO
+{
+    public class BigSister
+    {
+        public delegate void BigSisterDelegate(List<Product> cart, List<Product> receta);
+        public void Eliminar(List<Product> cart, List<Product> receta)
+        {
+            foreach( Product c in cart)
+            {
+                foreach(Product r in receta)
+                {
+                    if(r.Name == c.Name)
+                    {
+                        continue;
+                    }
+                    else { cart.Remove(c); }
+                }
+            }
+        }
+
+        public event BigSisterDelegate Reto;
+
+
+
+        
+
+
+    }
+
+
+}
--- LabPOO/Program.cs
+++ LabPOO/Program.cs
@@ -11,6 +11,7 @@ namespace LabPOO
     {
         public static List<Product> cart;
         public static List<Product> market;
+        public static List<Product> receta;
 
         static void Main(string[] args)
         {
@@ -94,6 +95,7 @@ namespace LabPOO
                     }
                     AddToCart(market[answer]);
                     break;
+                   
                 }
                 catch
                 {
@@ -137,28 +139,41 @@ namespace LabPOO
         public static void SupplyStore()
         {
             market.Add(new Product("Leche Entera", 820, 89, "1L"));
+            receta.Add(new Product("Leche Entera", 820, 89, "1L"));
             market.Add(new Product("Gomitas Flipy", 720, 12, "100g"));
             market.Add(new Product("Mantequilla", 850, 12, "125g"));
+            receta.Add(new Product("Mantequilla", 850, 12, "125g"));
             market.Add(new Product("Crema para hemorroides", 4990, 7, "300cc"));
             market.Add(new Product("Pimienta", 430, 84, "15g"));
+            receta.Add(new Product("Pimienta", 430, 84, "15g"));
             market.Add(new Product("Vino Sauvignon Blanc Reserva Botella", 4150, 23, "750cc"));
             market.Add(new Product("Sal Lobos", 330, 150, "1kg"));
+            receta.Add(new Product("Sal Lobos", 330, 150, "1kg"));
             market.Add(new Product("Cuaderno Mi Pequeño Pony", 1290, 50, "1un"));
             market.Add(new Product("Láminas de Lasaña", 1250, 85, "400g"));
+            receta.Add(new Product("Láminas de Lasaña", 1250, 85, "400g"));
             market.Add(new Product("Tomate", 1290, 200, "1kg"));
+            receta.Add(new Product("Tomate", 1290, 200, "1kg"));
             market.Add(new Product("Harina", 890, 43, "1kg"));
+            receta.Add(new Product("Harina", 890, 43, "1kg"));
             market.Add(new Product("Audifonos Samsung", 5990, 40, "1un"));
             market.Add(new Product("Pisco Alto del Carmen", 5990, 120, "1L"));
             market.Add(new Product("Carne Molida", 4390, 15, "500g"));
+            receta.Add(new Product("Carne Molida", 4390, 15, "500g"));
             market.Add(new Product("Aceite de Oliva", 1790, 77, "250g"));
+            receta.Add(new Product("Aceite de Oliva", 1790, 77, "250g"));
             market.Add(new Product("Sal parrillera", 840, 50, "750g"));
             market.Add(new Product("Cable HDMI 1m", 3990, 25, "1un"));
             market.Add(new Product("Queso Rallado Parmesano", 499, 102, "40g"));
+            receta.Add(new Product("Queso Rallado Parmesano", 499, 102, "40g"));
             market.Add(new Product("Vino Blanco Caja", 2790, 84, "2L"));
+            receta.Add(new Product("Vino Blanco Caja", 2790, 84, "2L"));
             market.Add(new Product("Malla de Cebollas", 1090, 91, "1kg"));
+            receta.Add(new Product("Malla de Cebollas", 1090, 91, "1kg"));
             market.Add(new Product("Tomates Pelados en lata", 700, 48, "540g"));
             market.Add(new Product("Queso Parmesano", 3790, 41, "200g"));
             market.Add(new Product("Bolsa de Zanahorias", 890, 74, "1un"));
+            receta.Add(new Product("Bolsa de Zanahorias", 890, 74, "1un"));
         }
 
         public static void ShowRecipe()
